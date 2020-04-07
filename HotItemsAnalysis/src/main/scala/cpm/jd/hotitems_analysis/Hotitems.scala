f4d9aae3299e8b8caf44bd5c4ea7ba03b6029b00
jojozx <18729198283@163.com>
2020-04-07 20:29:06 +0800

    简单kafka操作

--- HotItemsAnalysis/src/main/scala/cpm/jd/hotitems_analysis/Hotitems.scala
+++ HotItemsAnalysis/src/main/scala/cpm/jd/hotitems_analysis/Hotitems.scala
@@ -1,22 +1,29 @@
 package cpm.jd.hotitems_analysis
 
 
-
+import org.springframework.beans.factory.annotation.Value
 import org.apache.flink.streaming.api.scala._
-case  class UserBehavior(UserId :Long ,itemId :Long ,categoryId:Integer ,behavior:String,timestamp :Long )
-case class ItemViewCount(itemId :Long ,windowEnd:Long ,Count :Long)
-object Hotitems {
-  def main(args: Array[String]): Unit = {
-   val env =StreamExecutionEnvironment.getExecutionEnvironment
-    env.setParallelism(1)
 
-    val dataStream =env.readTextFile("D:\\workspac\\UserBehaviorAnalysis\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv")
-     val UserBeHaviorStream=dataStream.map(data =>{
-       val dataArray=data.split(",")
-       UserBehavior(dataArray(0).trim.toLong,dataArray(1).trim.toLong,dataArray(2).trim.toInt,
-         dataArray(3).trim(),dataArray(4).toLong)
-     })
-    UserBeHaviorStream.assignAscendingTimestamps(_.timestamp*1000L)
+
+case class UserBehavior(UserId: Long, itemId: Long, categoryId: Integer, behavior: String, timestamp: Long)
+
+case class ItemViewCount(itemId: Long, windowEnd: Long, Count: Long)
+
+object Hotitems {
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    @Value("${asd}")
+    var someThing : String=null
+
+    val dataStream = env.readTextFile("D:\\workspac\\UserBehaviorAnalysis\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv")
+    val UserBeHaviorStream = dataStream.map(data => {
+      val dataArray = data.split(",")
+      UserBehavior(dataArray(0).trim.toLong, dataArray(1).trim.toLong, dataArray(2).trim.toInt,
+        dataArray(3).trim(), dataArray(4).toLong)
+    })
+    UserBeHaviorStream.assignAscendingTimestamps(_.timestamp * 1000L)
     dataStream.print()
     env.execute("a1")
   }
